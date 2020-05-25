bcd10818778ed6e875a8950ada1f5c495af93faa
pranavv481 <pranavv481@gmail.com>
2020-05-25 14:54:44 +0530

    hookObject

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import HookCounterThree from './components/HookCounterThree';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HookCounterThree />
+      </div>
+    );
+  }
+}
+
+export default App;
